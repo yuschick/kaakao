60d1d9945008e53791f7ac944a22c80164ac1047
Daniel Yuschick <daniel.yuschick@gmail.com>
2023-08-20 12:49:01 +0300

    chore: fix linting

--- src/components/VisuallyHidden/VisuallyHidden.tsx
+++ src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -6,43 +6,43 @@ import styles from './VisuallyHidden.module.css';
 
 import { useMergeRefs } from '@hooks';
 
-export interface Props
-  extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | 'style'> {
-  children: React.ReactNode;
-  /**
-   * When the visually hidden content is focused or contains focus, this className will be toggled on the container.
-   */
-  classNameOnFocus?: string;
+export interface Props extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | 'style'> {
+    children: React.ReactNode;
+    /**
+     * When the visually hidden content is focused or contains focus, this className will be toggled on the container.
+     */
+    classNameOnFocus?: string;
 }
 
-export const VisuallyHidden = forwardRef<HTMLDivElement, Props>(
-  function VisuallyHidden({ children, classNameOnFocus, ...htmlAttributes }, externalRef) {
+export const VisuallyHidden = forwardRef<HTMLDivElement, Props>(function VisuallyHidden(
+    { children, classNameOnFocus, ...htmlAttributes },
+    externalRef,
+) {
     const internalRef = useRef<HTMLDivElement>(null);
     const ref = useMergeRefs([internalRef, externalRef]);
     const [hasFocus, setHasFocus] = useState<boolean>(false);
 
-        const handleBlur = () => {
-          setHasFocus(false);
-        };
+    const handleBlur = () => {
+        setHasFocus(false);
+    };
 
-                const handleFocus = (event: FocusEvent) => {
-                  if (event.target.parentElement === internalRef.current) {
-                    setHasFocus(true);
-                  }
-                };
+    const handleFocus = (event: FocusEvent) => {
+        if (event.target.parentElement === internalRef.current) {
+            setHasFocus(true);
+        }
+    };
 
-                return (
-      <div
-        className={classNames(styles.visuallyHidden, hasFocus ? classNameOnFocus : null, {
-          [styles.showOnFocus]: Boolean(classNameOnFocus),
-        })}
-        ref={ref}
-        onBlur={handleBlur}
-        onFocus={(event) => handleFocus(event)}
-        {...htmlAttributes}
-      >
-        {children}
-      </div>
+    return (
+        <div
+            className={classNames(styles.visuallyHidden, hasFocus ? classNameOnFocus : null, {
+                [styles.showOnFocus]: Boolean(classNameOnFocus),
+            })}
+            ref={ref}
+            onBlur={handleBlur}
+            onFocus={(event) => handleFocus(event)}
+            {...htmlAttributes}
+        >
+            {children}
+        </div>
     );
-  },
-);
+});
